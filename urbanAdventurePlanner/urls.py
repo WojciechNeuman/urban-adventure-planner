42f192fe7c85b4951d9e5eda5c848cf6629f1c45
Wojciech Neuman <wojciech.neuman@gmail.com>
2024-04-09 00:09:29 +0200

    Added confidential data safeguard. Back to initial commit with ready-to-use database, user authentification and basic design.

---
+++ urbanAdventurePlanner/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    # path('login', views.login, name='login'),
+    path('sign-up', views.sign_up, name='sign-up'),
+    path('profile', views.profile, name='profile-page'),
+    path('password_reset', views.password_reset, name='password_reset')
+
+]
+
